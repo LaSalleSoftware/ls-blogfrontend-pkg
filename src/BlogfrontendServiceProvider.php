1545ebca2e4a789468937ccc2765b199e0dd142e
Bob Bloom <bob.bloom@lasallesoftware.ca>
2019-07-11 11:14:35 -0400

    Gigantic commit for v1.9.2. #1

---
+++ src/BlogfrontendServiceProvider.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * This file is part of the Lasalle Software blog back-end package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  (c) 2019 The South LaSalle Trading Corporation
+ * @license    http://opensource.org/licenses/MIT
+ * @author     Bob Bloom
+ * @email      bob.bloom@lasallesoftware.ca
+ * @link       https://lasallesoftware.ca
+ * @link       https://packagist.org/packages/lasallesoftware/lsv2-blogbackend-pkg
+ * @link       https://github.com/LaSalleSoftware/lsv2-blogbackend-pkg
+ *
+ */
+
+namespace Lasallesoftware\Blogfrontend;
+
+// Laravel class
+// https://github.com/laravel/framework/blob/5.6/src/Illuminate/Support/ServiceProvider.php
+use Illuminate\Support\ServiceProvider;
+
+
+class BlogfrontendServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * "Within the register method, you should only bind things into the service container.
+     * You should never attempt to register any event listeners, routes, or any other piece of functionality within
+     * the register method. Otherwise, you may accidentally use a service that is provided by a service provider
+     * which has not loaded yet."
+     * (https://laravel.com/docs/5.6/providers#the-register-method(
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+
+    /**
+     * Bootstrap any package services.
+     *
+     * "So, what if we need to register a view composer within our service provider?
+     * This should be done within the boot method. This method is called after all other service providers
+     * have been registered, meaning you have access to all other services that have been registered by the framework"
+     * (https://laravel.com/docs/5.6/providers)
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutes();
+
+        //$this->loadTranslations();
+    }
+
+    /**
+     * Load this package's routes
+     *
+     * @return void
+     */
+    protected function loadRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/blog.php');
+    }
+
+    /**
+     * Load this package's translations
+     *
+     * @return void
+     */
+    protected function loadTranslations()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../translations/', 'lasallesoftwareblogbackend');
+    }
+
+}
---
+++ src/Http/Controllers/DisplaySinglePostController.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+ * This file is part of the Lasalle Software blog back-end package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  (c) 2019 The South LaSalle Trading Corporation
+ * @license    http://opensource.org/licenses/MIT
+ * @author     Bob Bloom
+ * @email      bob.bloom@lasallesoftware.ca
+ * @link       https://lasallesoftware.ca
+ * @link       https://packagist.org/packages/lasallesoftware/lsv2-blogbackend-pkg
+ * @link       https://github.com/LaSalleSoftware/lsv2-blogbackend-pkg
+ *
+ */
+
+namespace Lasallesoftware\Blogfrontend\Http\Controllers;
+
+// LaSalle Software
+use Lasallesoftware\Library\Common\Http\Controllers\CommonController;
+use Lasallesoftware\Library\UniversallyUniqueIDentifiers\UuidGenerator;
+
+
+// Third party classes
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key;
+
+class DisplaySinglePostController extends CommonController
+{
+    public function DisplaySinglePost()
+    {
+        $token = $this->createJWT();
+
+        $headers = [
+            'Authorization'   => 'Bearer ' . $token,
+            'InstalledDomain' => 1,
+            'Accept'          => 'application/json',
+        ];
+
+        $client = new Client();
+
+        //$getUrl = "http://temp.api.com:8888/api";
+        $getUrl = "http://hackintosh.lsv2-adminbackend-app.com:8888/singlearticleblog";
+
+        try {
+
+            $response = $client->request('GET', $getUrl, [
+                'headers'         => $headers,
+                'connect_timeout' => 10,
+            ]);
+
+            // Here the code for successful request
+            $body = json_decode($response->getBody());
+
+            echo "<h1>" . $getUrl . "</h1>";
+            echo "<h1>" . $response->getStatusCode() . "</h1>";
+
+            $this->viewPost($body->post, $body->tags);
+
+            $this->viewPostupdates($body->postupdates);
+
+            echo "<br><br>---- end of post! -----<br>";
+
+            echo "<h1>token = "  . $body->token;
+            echo "<br>domain = " . $body->domain;
+
+
+
+
+        } catch (RequestException $e) {
+
+            // BAD REQUEST
+            // The server cannot or will not process the request due to something that is perceived to be a client error
+            // (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).
+            // https://httpstatuses.com/400
+            if ($e->getResponse()->getStatusCode() == '400') {
+                echo "Got response 400 - Bad Request";
+            }
+
+            // UNAUTHORIZED
+            // The request has not been applied because it lacks valid authentication credentials for the target resource.
+            // https://httpstatuses.com/401
+            if ($e->getResponse()->getStatusCode() == '401') {
+                echo "Got response 401 - Unauthorized";
+            }
+
+            // FORBIDDEN
+            // The server understood the request but refuses to authorize it.
+            // https://httpstatuses.com/403
+            if ($e->getResponse()->getStatusCode() == '403') {
+                echo "Got response 404 - Forbidden";
+            }
+
+            // NOT FOUND
+            // The origin server did not find a current representation for the target resource or
+            // is not willing to disclose that one exists.
+            // https://httpstatuses.com/404
+            if ($e->getResponse()->getStatusCode() == '404') {
+                echo "Got response 404 - Not Found";
+            }
+
+        } catch (\Exception $e) {
+
+            // There was another exception.
+            echo "No response was received. No status code nor any diagnostic information was given to us.";
+
+        }
+    }
+
+    public function viewPost($post, $tags)
+    {
+        echo (is_null($post->featured_image)) ? "<br>(there is no featured_image)" : "<br>'('.featured_image: ".$post->featured_image.')';
+        echo "<h1>" . $post->title . "</h1>";
+        echo "(slug = " . $post->slug . ")";
+        echo "<br>by " . $post->author;
+        echo "<br>"  .$post->date;
+        echo (is_null($post->category_name)) ? '' : "<br>category: " . $post->category_name;
+        echo (is_null($tags)) ? '' : $this->viewTags($tags) ;
+        echo "<br><br>";
+        echo "(excerpt: " . $post->excerpt . ")";
+        echo "<br><br>(meta_description: " . $post->meta_description . ")";
+        echo "<br><br>" .  $post->content;
+
+    }
+
+    public function viewTags($tags)
+    {
+        $counter = 1;
+        $numberOfTags = count($tags);
+        echo "<br>tags: ";
+        foreach ($tags as $tag) {
+
+            echo $tag->title;
+            if ($counter < $numberOfTags) {
+                echo ", ";
+            }
+            $counter++;
+        }
+    }
+
+    public function viewPostupdates($postupdates)
+    {
+        if (!is_null($postupdates)) {
+
+            (count($postupdates) == 1) ? $word = "is" : $word = "are";
+
+            echo "<br><h2>There " . $word . " " . count($postupdates) ." Updates For This Post!</h2>";
+
+            foreach ($postupdates as $postupdate) {
+                echo "<h3>" . $postupdate->title . "</h3>";
+                echo $postupdate->date;
+                echo "<br>(excerpt: " . $postupdate->excerpt . ")";
+                echo "<br>" . $postupdate->content;
+            }
+        }
+    }
+
+
+    public function createJWT()
+    {
+        $signer           = new Sha256();
+        $time             = time();
+        $installed_domain = app('config')->get('lasallesoftware-library.lasalle_app_domain_name');
+
+
+        // https://auth0.com/docs/tokens/jwt-claims
+        // The JWT specification defines seven reserved claims that are not required, but are recommended to allow interoperability with third-party applications. These are:
+        //
+        // iss (issuer): Issuer of the JWT
+        // sub (subject): Subject of the JWT (the user)
+        // aud (audience): Recipient for which the JWT is intended
+        // exp (expiration time): Time after which the JWT expires
+        // nbf (not before time): Time before which the JWT must not be accepted for processing
+        // iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+        // jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+        $token = (new Builder())
+            ->issuedBy($installed_domain)              // Configures the issuer (iss claim)
+            //->permittedFor('admin')                  // Configures the audience (aud claim)
+            ->identifiedBy($this->makeUuid(), true)    // Configures the id (jti claim), replicating as a header item
+            ->issuedAt($time)                          // Configures the time that the token was issue (iat claim)
+            ->canOnlyBeUsedAfter($time + 60)           // Configures the time that the token can be used (nbf claim)
+            ->expiresAt($time + 3600)                  // Configures the expiration time of the token (exp claim)
+            ->getToken($signer, new Key('testing'))    // Retrieves the generated token
+        ;
+
+        //var_dump($token->verify($signer, 'testing 1'));  // false, because the key is different
+        //var_dump($token->verify($signer, 'testing'));    // true, because the key is the same
+
+        return $token;
+    }
+
+    public function makeUuid(UuidGenerator $uuidGenerator)
+    {
+        return $uuidGenerator->newUuid();
+    }
+}
