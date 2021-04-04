c4782b18363a20a6ccc9409a146b4e6a0295b830
Bob Bloom <bob.bloom@lasallesoftware.ca>
2021-04-03 21:53:22 -0400

    Display recent vids and podcasts on the home page. #41

--- src/Http/Controllers/DisplayHomepageBlogPostsController.php
+++ src/Http/Controllers/DisplayHomepageBlogPostsController.php
@@ -45,33 +45,44 @@ class DisplayHomepageBlogPostsController extends BaseFrontendController
 
 
     public function DisplayHomepageBlogPosts()
-    { 
+    {
         $endpointPath = $this->getEndpointPath('DisplayHomepageBlogPostsController');
-        $httpRequest  = 'GET';
+        $httpRequest  = 'POST';
 
-        $response = $this->sendRequestToLasalleBackend($endpointPath, $httpRequest);
+        $postData = [
+            'podcast_shows'              => config('lasallesoftware-libraryfrontend.lasalle_podcast_shows_to_display_on_the_home_page'),
+            'number_of_podcast_episodes' => config('lasallesoftware-libraryfrontend.lasalle_number_of_recent_podcast_episodes_to_display_on_the_home_page'),
+            'video_shows'                => config('lasallesoftware-libraryfrontend.lasalle_video_shows_to_display_on_the_home_page'),
+            'number_of_video_episodes'   => config('lasallesoftware-libraryfrontend.lasalle_number_of_recent_video_episodes_to_display_on_the_home_page'),            
+        ];
+
+        $response = $this->sendRequestToLasalleBackend($endpointPath, $httpRequest, null, $postData);
 
         if (!isset($this->messages)) {
-
             $body = json_decode($response->getBody());
 
-            $transformedPosts = $this->getTransformedPosts($body->posts);
-
-            
+            $transformedPosts           = $this->getTransformedPosts($body->posts);
+            $transformedPodcastEpisodes = $this->getTransformedPodcastEpisodes($body->podcast_episodes);
+            $transformedVideoEpisodes   = $this->getTransformedVideoEpisodes($body->video_episodes);
 
         } else {
             $transformedPosts = false;
-        }
+        } 
+
 
         return view(config('lasallesoftware-libraryfrontend.lasalle_path_to_front_end_view_path') . '.home', [
             'posts'                                => $transformedPosts,
             'numberOfPosts'                        => ($transformedPosts) ? count($transformedPosts) : 0,
+            'podcast_episodes'                     => $transformedPodcastEpisodes,
+            'video_episodes'                       => $transformedVideoEpisodes,
             'copyright'                            => env('LASALLE_COPYRIGHT_IN_FOOTER'),
             'socialMediaMetaTags'                  => $this->getSocialMediaMetaTags(),
             'featured_image_social_media_meta_tag' => config('lasallesoftware-libraryfrontend.lasalle_social_media_meta_tag_default_image'),
             'question'                             => $this->getSecurityQuestion(),
         ]);
     }
+
+
 
     private function getTransformedPosts($posts)
     {
@@ -94,6 +105,76 @@ class DisplayHomepageBlogPostsController extends BaseFrontendController
 
         return $transformedPosts;
     }
+
+
+    private function getTransformedPodcastEpisodes($podcastEpisodesForAllPodcastShows)
+    {
+        // The podcast show array contains its episodes to be displayed on the home page.
+        // So there are two nested foreach statements: the foreach show, and within this the foreach episodes.
+
+        // Cycle through each podcast show
+        $transformedPodcastEpisodesForAllPodcastShows = [];
+        foreach ($podcastEpisodesForAllPodcastShows as $podcastEpisodeForAPodcastShow) {
+
+            // cycle through each podcast show's episodes, transforming each episode one-by-one
+            $transformedPodcastEpisodeForAPodcastShow = [];
+            foreach ($podcastEpisodeForAPodcastShow as $podcastEpisode) {
+
+                $transformedEpisode = [
+                    'podcast_show_id'         => $podcastEpisode->podcast_show_id,
+                    'title'                   => $podcastEpisode->title,
+                    'website_excerpt'         => $podcastEpisode->website_excerpt,
+                    'website_featured_image'  => $podcastEpisode->website_featured_image,
+                    'itunes_link'             => $podcastEpisode->itunes_link,
+                    'website_publish_on'      => $this->formatDate($podcastEpisode->website_publish_on),
+                    ];                
+            
+                // add the transformed episode to the podcast show's array
+                $transformedPodcastEpisodeForAPodcastShow[] = $transformedEpisode;
+            }
+
+            // Add all the transformed episodes to (what is essentially) the podcast show "outer" array.
+            // So we've recreated the same nested array structure that we started with, except now we have transformed episodes.
+            $transformedPodcastEpisodesForAllPodcastShows[] = $podcastEpisodeForAPodcastShow;
+        }        
+
+        return $transformedPodcastEpisodesForAllPodcastShows;
+    }
+
+
+    private function getTransformedVideoEpisodes($videoEpisodesForAllVideoShows)
+    {
+        // The video show array contains its episodes to be displayed on the home page.
+        // So there are two nested foreach statements: the foreach show, and within this the foreach episodes.
+
+        // Cycle through each video show
+        $transformedVideoEpisodesForAllVideoShows = [];
+        foreach ($videoEpisodesForAllVideoShows as $videoEpisodeForAVideoShow) {
+
+            // cycle through each video show's episodes, transforming each episode one-by-one
+            $transformedVideoEpisodeForAVideoShow = [];
+            foreach ($videoEpisodeForAVideoShow as $videoEpisode) {
+
+                $transformedEpisode = [
+                    'video_show_id'           => $videoEpisode->video_show_id,
+                    'title'                   => $videoEpisode->title,
+                    'website_excerpt'         => $videoEpisode->website_excerpt,
+                    'website_featured_image'  => $videoEpisode->website_featured_image,
+                    'website_publish_on'      => $this->formatDate($videoEpisode->website_publish_on),
+                    ];                
+            
+                // add the transformed episode to the podcast show's array
+                $transformedVideoEpisodeForAVideoShow[] = $transformedEpisode;
+            }
+
+            // Add all the transformed episodes to (what is essentially) the video show "outer" array.
+            // So we've recreated the same nested array structure that we started with, except now we have transformed episodes.
+            $transformedVideoEpisodesForAllVideoShows[] = $transformedVideoEpisodeForAVideoShow;
+        }        
+
+        return $transformedVideoEpisodesForAllVideoShows;
+    }
+    
 
     /**
      * Get the social media meta tags.
